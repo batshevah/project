a74be56967275519822e7280e1995e0eb6b8b2a1
batshevah <40803509+batshevah@users.noreply.github.com>
2018-07-04 09:52:20 +0300

    Update saveDNDprefer.php

--- saveDNDprefer.php
+++ saveDNDprefer.php
@@ -1,7 +1,7 @@
 <?php 
 $prefer=$_GET["prefer"];
  $ix=$_GET["ix"];
- $query = "update preference set prefer='{$prefer}' where ix='{$ix}'";
+ $query = "update preferences set preference='{$prefer}' where ix='{$ix}'";
  $GLOBALS['myCon']->query($query);
  header('Content-Type:text/plain');
  echo $prefer;
